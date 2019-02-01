a38b5d62afe93b8b92e17e4f97881fdd64463673
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-02-01 05:34:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     response = 'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase == 'DOGS'
+  elsif true phrase.upcase
     response = 'NO, NOT SINCE 1938!'
   else
     response = 'HUH?! SPEAK UP, SONNY!'
